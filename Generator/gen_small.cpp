4943637398355ddeeb35a7e1c7cb9bc4296afa31
NUR ALAM JOY <87431577+nuralamjoy@users.noreply.github.com>
2026-06-25 21:32:01 +0600

    create random test case generator cpp code

---
+++ Generator/gen_small.cpp
@@ -0,0 +1,19 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+
+    int n = rnd.next(2, 1000);
+
+    cout << n << "\n";
+
+    for(int i = 0; i < n; i++) {
+        cout << rnd.next(1LL, 1000000000LL);
+        if(i + 1 < n) cout << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
