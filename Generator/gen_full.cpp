2c1ab9bcc40ea9c0291d6d4502d783b65fcee6f1
NUR ALAM JOY <87431577+nuralamjoy@users.noreply.github.com>
2026-06-25 21:39:06 +0600

    Update gen_full.cpp

--- Generator/gen_full.cpp
+++ Generator/gen_full.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
 
-    int n = rnd.next(2, 1000);
+    int n = rnd.next(2, 1000);    //inside the bracket the array size min size to max size
 
     cout << n << "\n";
 
     for(int i = 0; i < n; i++) {
-        cout << rnd.next(1LL, 1000000000LL);
+        cout << rnd.next(1LL, 1000000000LL);    
         if(i + 1 < n) cout << ' ';
     }
     cout << '\n';
