c3cea40d58afbc6065f68920dc4b3996c7228e4c
NUR ALAM JOY <87431577+nuralamjoy@users.noreply.github.com>
2026-06-25 21:47:11 +0600

    Update script.cpp

--- Generator/script.cpp
+++ Generator/script.cpp
@@ -3,3 +3,4 @@ gen > $
 </#list>
 // thise file write on script file on polygon
 //suppose we created 3 test case for showw ojn the statement ,but we need 50 test case ,thats case it would be 4..50
+// for every generator file scipt code write for each generator file
