fd7d378d9dd1df8b9556cb7f3831904cd3e2c7aa
NUR ALAM JOY <87431577+nuralamjoy@users.noreply.github.com>
2026-06-27 21:16:52 +0600

    for large size string generate

---
+++ Generator/lrg_gen.cpp
@@ -0,0 +1,16 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+
+    int n = rnd.next(1001, 10000);
+
+    cout << n << "\n";
+
+    for (int i = 0; i < n; i++)
+        cout << char(rnd.next('a', 'z'));
+
+    cout << "\n";
+}
