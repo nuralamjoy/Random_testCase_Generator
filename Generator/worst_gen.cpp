451ab842b9155412058f0881093c82a258e430e1
NUR ALAM JOY <87431577+nuralamjoy@users.noreply.github.com>
2026-06-27 21:17:33 +0600

    for worst size string generate

---
+++ Generator/worst_gen.cpp
@@ -0,0 +1,11 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 10000;
+    cout << n << "\n";
+    for(int i=0;i<n;i++)
+        cout << 'a';
+    cout << "\n";
+}
